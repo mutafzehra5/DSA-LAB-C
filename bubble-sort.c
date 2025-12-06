d18670021c9935ccfefbecde0d2eb2205d25b4ec
Mutaf zehra <MUTAAFZEHRA@GMAIL.COM>
2025-12-06 12:52:03 +0530

    fix errors

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,36 +1,48 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void bubbleSort(int arr[], int size);
 
-int main (){
-  int size = 10;
-  int arr[size];
-  printf("Enter array entries: ");
-  for(int i = 0; i < size; i++){
-      scanf("%d", &arr[i]);
-  }
+int main()
+{
+    int size = 10;
+    int arr[size];
+    printf("Enter array entries: ");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
 
-  printf("Entered array is: );
-  for(int i = 0; i < size; i++){
-    printf("%d ", arr[i]);
-  }
+    printf("Entered array is: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 
-  printf("Sorted array is: );
-  bubbleSort(arr, size);
-  return 0;
+    bubbleSort(arr, size);
+
+    return 0;
 }
 
-void bubbleSort(int arr[], int size){
-  for (int i = 0; i < size; i++){
-    for(int j = 0; j < size; j++){
-      if (arr[j] > arr[j+1]){
-        arr[j] = arr[j+1];
-      }
+void bubbleSort(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
     }
-  }
 
-  printf("Sorted array is: );
-  for(int i = 0; i < size; i++){
-    printf("%d ", arr[i]);
-  }
+    printf("Sorted array is: ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
